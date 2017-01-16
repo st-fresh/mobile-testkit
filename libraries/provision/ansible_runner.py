fb9fe13cc914433bd00702ca0ee1a34a8e8cf7f7
Seth Rosetter <seth.rosetter@gmail.com>
2017-01-16 14:10:41 -0800

    Add must_run_playbook and fix possible side effects due to default mutable args.

--- libraries/provision/ansible_runner.py
+++ libraries/provision/ansible_runner.py
@@ -2,6 +2,8 @@ from ansible_python_runner import Runner
 from ansible import constants
 import logging
 
+from keywords import exceptions
+
 PLAYBOOKS_HOME = "libraries/provision/ansible/playbooks"
 
 
@@ -10,7 +12,10 @@ class AnsibleRunner:
     def __init__(self, config):
         self.provisiong_config = config
 
-    def run_ansible_playbook(self, script_name, extra_vars={}, subset=constants.DEFAULT_SUBSET):
+    def run_ansible_playbook(self, script_name, extra_vars=None, subset=constants.DEFAULT_SUBSET):
+
+        if extra_vars is None:
+            extra_vars = {}
 
         inventory_filename = self.provisiong_config
 
@@ -28,3 +33,13 @@ class AnsibleRunner:
         logging.info(stats)
 
         return len(stats.failures) + len(stats.dark)
+
+    def must_run_playbook(self, script_name, extra_vars=None):
+        """ Run an ansible playbook and will raise an exception if any errors occur."""
+
+        if extra_vars is None:
+            extra_vars = {}
+
+        num_errors = self.run_ansible_playbook(script_name=script_name, extra_vars=extra_vars)
+        if num_errors != 0:
+            raise exceptions.ProvisioningError("Ansible runner failed while executing: {}".format(script_name))
