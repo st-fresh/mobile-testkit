e72063ee622960e4b33b6a2832d9cd6dcdab8af8
Traun Leyden <tleyden@couchbase.com>
2017-02-03 15:49:06 -0800

    Add cluster configs with load balancers

--- libraries/utilities/generate_clusters_from_pool.py
+++ libraries/utilities/generate_clusters_from_pool.py
@@ -409,7 +409,9 @@ def generate_clusters_from_pool(pool_file):
         ClusterDef("1sg_1cbs_1lgs", num_sgs=1, num_acs=0, num_cbs=1, num_lgs=1, num_lbs=0),
         ClusterDef("2sg_3cbs_2lgs", num_sgs=2, num_acs=0, num_cbs=3, num_lgs=2, num_lbs=0),
         ClusterDef("1sg_1ac_1cbs_1lgs", num_sgs=1, num_acs=1, num_cbs=1, num_lgs=1, num_lbs=0),
+        ClusterDef("1sg_1ac_1cbs_1lgs_1lbs", num_sgs=1, num_acs=1, num_cbs=1, num_lgs=1, num_lbs=1),
         ClusterDef("2sg_2ac_3cbs_1lgs", num_sgs=2, num_acs=2, num_cbs=3, num_lgs=1, num_lbs=0),
+        ClusterDef("2sg_2ac_3cbs_1lgs_1lbs", num_sgs=2, num_acs=2, num_cbs=3, num_lgs=1, num_lbs=1),
         ClusterDef("2sg_2ac_3cbs_2lgs", num_sgs=2, num_acs=2, num_cbs=3, num_lgs=2, num_lbs=0),
         # Perf Mini Matrix
         ClusterDef("1sg_1ac_3cbs_1lgs", num_sgs=1, num_acs=1, num_cbs=3, num_lgs=1, num_lbs=0),
